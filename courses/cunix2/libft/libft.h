ac7cffff38c1ba0e82887727b1d94f1f61d506bc
maxpushka <maxpushka200@gmail.com>
2021-08-09 18:56:26 +0300

    done ft_strncmp

--- courses/cunix2/libft/libft.h
+++ courses/cunix2/libft/libft.h
@@ -10,4 +10,6 @@ size_t ft_strlen(const char *s);
 char *ft_strcpy(char *dest, const char *src);
 char *ft_strdup(const char *s);
 
+int ft_strncmp(const char *s1, const char *s2, size_t n);
+
 #endif
---
+++ courses/cunix2/libft/src/ft_strncmp.c
@@ -0,0 +1,18 @@
+#include "../libft.h"
+
+int ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        if (s1[i] > s2[i])
+        {
+            return 1;
+        }
+        else if (s1[i] < s2[i])
+        {
+            return -1;
+        }
+    }
+
+    return 0;
+}
