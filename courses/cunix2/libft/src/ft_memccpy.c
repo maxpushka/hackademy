10157ff98e9a874d811b9de85ad58e52eac9f5b5
maxpushka <maxpushka200@gmail.com>
2021-08-10 12:00:22 +0300

    done ft_memccpy; updated ft_memcpy

--- courses/cunix2/libft/src/ft_memccpy.c
+++ courses/cunix2/libft/src/ft_memccpy.c
@@ -1,12 +1,17 @@
 #include "../libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void *ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
     char *ch_dest = (char *) dest;
     const char *ch_src = (char *) src;
 
     for (size_t i = 0; i < n; i++)
     {
+        if (ch_src[i] == (char) c)
+        {
+            break;
+        }
+
         ch_dest[i] = ch_src[i];
     }
 
